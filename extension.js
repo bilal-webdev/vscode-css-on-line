4afdde2b14fe1115ff4b4eda68f6270ecd8c6815
bilal-webdev <bilalshaikh9598@gmail.com>
2023-06-04 17:02:48 +0530

    logic updated

--- extension.js
+++ extension.js
@@ -1,71 +1,65 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fs = require("fs");
 
-const gulp = require("gulp");
-const { compressCss } = require("gulp-css-format-oneline");
+function formatCSSFile(inputFilePath) {
+  const cssContent = fs.readFileSync(inputFilePath, "utf8");
+  // Remove leading and trailing white spaces
+  const formattedCSS = cssContent.trim();
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+  // Format the CSS by removing unnecessary line breaks and extra spaces
+  let updatedCSS = formattedCSS
+    .replace(/\s*\{\s*/g, " {")
+    .replace(/\s*\}\s*/g, "}\n")
+    .replace(/\;\s*/g, ";")
+    .replace(/\n\s*\n/g, "\n");
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+  updatedCSS = updatedCSS.replace(/(@keyframes|@media)\s*[^{]*{/g, (match) => {
+    return match.replace("{", "{\n");
+  });
 
-function formatCSSFile(inputFilePath, outputFilePath) {
-  return gulp
-    .src(inputFilePath)
-    .pipe(compressCss({ clearLine: false }))
-    .pipe(gulp.dest(outputFilePath));
-}
+  // Write the formatted CSS back to the same file
+  fs.writeFileSync(inputFilePath, updatedCSS, "utf8");
 
-function formatActiveCSS() {
-  const editor = vscode.window.activeTextEditor;
-
-  if (editor && editor.document.languageId === "css") {
-    const inputFilePath = editor.document.uri.fsPath;
-    const outputFilePath = inputFilePath; // overwrite the original file
-
-    formatCSSFile(inputFilePath, outputFilePath)
-      .on("end", () => {
-        vscode.window.showInformationMessage("CSS file formatted");
-      })
-      .on("error", (err) => {
-        vscode.window.showErrorMessage(
-          `Error formatting CSS file: ${err.message}`
-        );
-      });
-  } else {
-    vscode.window.showInformationMessage("No active CSS editor found");
-  }
+  return updatedCSS;
 }
 
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "css-on-line" is now active!');
+  // Register CSS formatter
+  vscode.languages.registerDocumentFormattingEditProvider("css", {
+    provideDocumentFormattingEdits(document) {
+      const inputFilePath = document.uri.fsPath;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
+      formatCSSFile(inputFilePath);
+    },
+  });
+
   let disposable = vscode.commands.registerCommand(
     "css-on-line.cssOnLine",
-    function () {
-      // The code you place here will be executed every time your command is executed
-      formatActiveCSS();
+    () => {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+      if (editor && editor.document.languageId === "css") {
+        // Get the document and its content
+        const document = editor.document;
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from CSS On Line!");
+        // Get the file path
+        const filePath = document.fileName;
+
+        formatCSSFile(filePath);
+
+        vscode.window.showInformationMessage("CSS file formatted");
+      } else {
+        vscode.window.showInformationMessage("No active CSS editor found");
+      }
     }
   );
 
+  console.log('Congratulations, your extension "css-on-line" is now active!');
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
-
 module.exports = {
   activate,
-  deactivate,
 };
